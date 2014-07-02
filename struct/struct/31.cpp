e20146fc9a80b043a8d27bd38e99c6f1aeca12cf
JUSTIVE <ben399399@naver.com>
2014-07-02 17:12:14 +0900

    c studing 140702
    c studing 140702

---
+++ struct/struct/31.cpp
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+int set_human(struct TEST a, int age, int gender);
+struct TEST 
+{
+    int age;
+    int gender;
+};
+int main()
+{
+    struct TEST human;
+
+    set_human(human, 10, 1);
+
+    printf("AGE : %d // Gender : %d ", human.age, human.gender);
+    return 0;
+}
+int set_human(struct TEST a, int age, int gender)
+{
+    a.age = age;
+    a.gender = gender;
+
+    return 0;
+}
